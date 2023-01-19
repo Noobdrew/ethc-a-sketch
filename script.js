eea92b3c52e6de938ad4be2abe2f37580bd94066
Noobdrew <noobdrew23@gmail.com>
2023-01-19 18:32:03 +0200

    color and rainbow and clear buttons work
    need to make it look better and add eraser maybe

--- script.js
+++ script.js
@@ -3,6 +3,11 @@ const container = document.querySelector('.container')
 let colorDiv = document.querySelector('.grid-element')
 let amount = document.querySelector('#range').value
 let rangeText = document.querySelector('.rangeText')
+
+let mouseDown = false
+document.body.onmousedown = () => mouseDown = true
+document.body.onmouseup = () => mouseDown = false
+let drawMode = 'color'
 createDiv()
 
 //create a new div and append it to container on call
@@ -22,25 +27,57 @@ function createDiv(e) {
 
 //create multuple divs
 
-
-console.log(amount)
-
 document.querySelector('#range').addEventListener('change', createDiv)
 document.querySelector('#range').addEventListener('input', slideValue)
 container.addEventListener('mouseover', changeColor)
 
 
-function slideValue() {
 
+function slideValue() {
     amount = document.querySelector('#range').value
     rangeText.textContent = `${amount}X${amount}`
-    console.log(amount)
+
 }
 
+//color divs
 function changeColor(e) {
-console.log(e.target)
-    e.target.style.cssText = `background-color: ${color.value}`
+    if (mouseDown==false) {
+        return
+
+    } else if (drawMode == 'color') {
+        
+        e.target.style.cssText = `background-color: ${color.value}`
+    } else if (drawMode == 'rainbow') {
+        let randomR = Math.floor(Math.random() * 256)
+        let randomG = Math.floor(Math.random() * 256)
+        let randomB = Math.floor(Math.random() * 256)
+        e.target.style.cssText = `background-color: rgb(${randomR}, ${randomG},${randomB} )`
+    }
+
+
 
 }
+
 let color = document.querySelector('#color')
 
+let buttonClear = document.querySelector('.clear')
+let buttonRainbow = document.querySelector('.rainbow')
+let buttonColor = document.querySelector('.colorBtn')
+buttonClear.addEventListener('click', clear)
+
+function clear(e) {
+    container.innerHTML = ''
+    createDiv()
+}
+
+
+buttonRainbow.addEventListener('click', rainbowDraw)
+buttonColor.addEventListener('click', colorDraw)
+
+function rainbowDraw() {
+    drawMode = 'rainbow'
+}
+
+function colorDraw() {
+    drawMode = 'color'
+}
