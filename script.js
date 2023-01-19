72992349747e4b2f89f57bbfcf2e56458b6dccda
Noobdrew <noobdrew23@gmail.com>
2023-01-19 16:52:17 +0200

    can color divs on mouseover
    need do add color change

--- script.js
+++ script.js
@@ -1,27 +1,20 @@
 //set DOM elements to variables
 const container = document.querySelector('.container')
-let gridElement = document.createElement('div')
+let colorDiv = document.querySelector('.grid-element')
 let amount = document.querySelector('#range').value
 let rangeText = document.querySelector('.rangeText')
-
-gridElement.classList.add('grid-element')
-
-
-
-console.log(gridElement)
-console.log(container)
+createDiv()
 
 //create a new div and append it to container on call
 function createDiv(e) {
     slideValue()
     container.textContent = ''
-    for (let i = 0; !(amount*amount == i); i++) {
-        
+    for (let i = 0; !(amount * amount == i); i++) {
+
         e = document.createElement('div')
         e.classList.add('grid-element')
         container.appendChild(e)
         container.style.cssText = `grid-template-columns: repeat(${amount}, 1fr); grid-template-rows: repeat(${amount}, 1fr)`
-
     }
 
 
@@ -34,18 +27,20 @@ console.log(amount)
 
 document.querySelector('#range').addEventListener('change', createDiv)
 document.querySelector('#range').addEventListener('input', slideValue)
+container.addEventListener('mouseover', changeColor)
 
 
 function slideValue() {
-    
+
     amount = document.querySelector('#range').value
     rangeText.textContent = `${amount}X${amount}`
     console.log(amount)
 }
 
-/* grid-template-columns: repeat(2, 1fr);
-  grid-template-rows: repeat(2, 1fr); */
+function changeColor(e) {
+    
+    e.target.classList.add('colored')
 
-function makeGrid() {
-    container.style.cssText = `grid-template-columns: repeat(${amount}, 1fr); grid-template-rows: repeat(${amount}, 1fr);`
 }
+
+console.log(document.querySelector('#color').value)
